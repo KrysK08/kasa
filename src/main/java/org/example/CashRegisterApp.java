c4bb10ba9cb40c552726bab6c1ced0389a85b378
krysia <krysiaak123@gmail.com>
2025-01-15 13:20:32 +0100

    skanowanie produktow

--- src/main/java/org/example/CashRegisterApp.java
+++ src/main/java/org/example/CashRegisterApp.java
@@ -1,7 +1,43 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CashRegisterApp {
+    private static ShopDatabase shopDatabase = new ShopDatabase();
+    private static List<Product> productList = new ArrayList<>();
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Rozpocznij skanowanie");
+        System.out.print("Wprowadź kod produktu: ");
+
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            processBarcode(input);
+            displayProductList();
+        }
+    }
+
+    private static void processBarcode(String barcode) {
+        Product product = shopDatabase.findProductByBarcode(barcode);
+
+        if (product != null) {
+            productList.add(product);
+            System.out.println("Dodano produkt: " + product.name);
+        } else {
+            System.out.println("Nie znaleziono produktu o kodzie: " + barcode);
+        }
+    }
+
+    private static void displayProductList() {
+        System.out.println("Aktualna lista zakupów:");
+        for (Product product : productList) {
+            System.out.println(product);
+        }
     }
 }
--- src/main/java/org/example/ShopDatabase.java
+++ src/main/java/org/example/ShopDatabase.java
@@ -8,14 +8,15 @@ class ShopDatabase {
 
     public ShopDatabase() {
         products = new ArrayList<>();
-        products.add(new Product("Mleko", "12345", 3.50, 0));
-        products.add(new Product("Chleb", "54321", 2.00, 0));
-        products.add(new Product("Masło", "11111", 6.00, 0));
+        products.add(new Product("Mleko", "12345/n", 3.50, 0));
+        products.add(new Product("Chleb", "54321/n", 2.00, 0));
+        products.add(new Product("Masło", "11111/n", 6.00, 0));
     }
 
     public Product findProductByBarcode(String barcode) {
         for (Product product : products) {
             if (product.barcode.equals(barcode)) {
+                product.quantity++;
                 return product;
             }
         }
